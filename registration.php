300f210b84c36013029b6021c24dde602cbda7c2
Toni Cortés <info@tonicort.es>
2020-11-11 12:29:37 +0000

    Custom shipping method

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * OtterCode_CustomShipping
+ *
+ * @category   OtterCode
+ * @package    OtterCode_CustomShipping
+ * @copyright  Copyright (c) 2020 OtterCode
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'OtterCode_CustomShipping',
+    __DIR__
+);
